e5ccfed4360d0653b5a7819f928e8970279740e0
Alisson Guedes <alissonguedes87@gmail.com>
2020-12-17 15:09:26 -0300

    Atualização

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Home Page')
+
+@section('sidebar')
+	
+	@parent
+
+@endsection
+
+@section('content')
+	Página Inicial
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\XMLImporterController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('/', [XMLImporterController::class, 'index']);
+Route::post('/', [XMLImporterController::class, 'import']);
